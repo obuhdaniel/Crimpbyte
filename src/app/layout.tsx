e4ff0150d9924f2a52a1690128f84d807a7942ca
obuhdaniel <danielobuh05@gmail.com>
2024-12-06 20:59:12 +0100

    added crypto page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className=''>
+      <body className='overflow-x-hidden mx-auto'>
         <NavBar />
         {children}
         <Footer />
